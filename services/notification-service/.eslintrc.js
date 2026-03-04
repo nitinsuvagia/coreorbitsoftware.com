ae0c67985506aa7b98bb64b310348cfb2f97b91c
Nitin Suvagiya <nitin@softqubes.com>
2026-03-04 21:09:10 +0530

    feat: Add GitHub Actions EC2 deployment with prerequisites
    - Add deploy-ec2.yml workflow with auto-install of Docker, Docker Compose, Git
    - Add ESLint configs for all 11 services (fixes CI lint errors)
    - Update docker-compose.prod.yml with build directives
    - Remove unused deploy.yml (ECS) and infrastructure.yml (Terraform)
    - Add prerequisite checks: Docker, Docker Compose, Git, disk space, memory

---
+++ services/notification-service/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    'prefer-const': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
